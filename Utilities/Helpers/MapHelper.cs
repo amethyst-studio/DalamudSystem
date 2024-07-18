ff1ef1601ab194623e0172c8114cd41d7b4a6686
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-07-18 17:10:31 -0400

    feat: convert MapToWorldCoord to float

--- Utilities/Helpers/MapHelper.cs
+++ Utilities/Helpers/MapHelper.cs
@@ -10,15 +10,15 @@ namespace DalamudSystem.Utilities.Helpers
 {
     public static class MapHelper
     {
-        public static (int X, int Y) MapToWorldCoordinates(Vector2 pos, uint mapId)
+        public static (float X, float Y) MapToWorldCoordinates(Vector2 pos, uint mapId)
         {
             ushort scale = ICoreManager.DataManager.GetExcelSheet<Map>()?.GetRow(mapId)?.SizeFactor ?? 100;
             float num = scale / 100f;
             float x = (float)(((pos.X - 1.0) * num / 41.0 * 2048.0) - 1024.0) / num * 1000f;
             float y = (float)(((pos.Y - 1.0) * num / 41.0 * 2048.0) - 1024.0) / num * 1000f;
-            x = (int)(MathF.Round(x, 3, MidpointRounding.AwayFromZero) * 1000) * 0.001f / 1000f;
-            y = (int)(MathF.Round(y, 3, MidpointRounding.AwayFromZero) * 1000) * 0.001f / 1000f;
-            return ((int)x, (int)y);
+            x = (MathF.Round(x, 1, MidpointRounding.AwayFromZero) * 1000) * 0.001f / 1000f;
+            y = (MathF.Round(y, 1, MidpointRounding.AwayFromZero) * 1000) * 0.001f / 1000f;
+            return (x, y);
         }
     }
 }
